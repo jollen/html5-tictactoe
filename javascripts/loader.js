31b619d219a9f02b5799ad0dc25a8f36476a732f
Jollen Chen <jollen@moko365.com>
2014-07-27 10:51:36 +0800

    install backbone, #1

--- javascripts/loader.js
+++ javascripts/loader.js
@@ -1,17 +1,25 @@
 requirejs.config({
 	baseUrl: 'bower_components/',
 	paths: {
-		'jquery': 'jquery/jquery.min'
-	}
+		'jquery': 'jquery/jquery.min',
+		'backbone': 'backbone/backbone',
+		'underscore': 'underscore/underscore'
+	},
+   	shim: {
+	    'backbone': {
+	        deps: ['jquery', 'underscore'],
+	        exports: 'Backbone'
+	    }
+    }
 });
 
 require([
 	/* by order */
 	'jquery',
-	'bower_components/jquery-ui/ui/jquery-ui.js',
-	'bower_components/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js',
-	'javascripts/main.js'
+	//'bower_components/jquery-ui/ui/jquery-ui.js',
+	//'bower_components/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js',
+	'backbone',
+	'javascripts/main.js'		// Game module
 ], function () {
-	console.log('Loaded.');
-	$('.grid').playRound();
+
 });
