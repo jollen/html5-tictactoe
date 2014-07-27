ce99f1581a15101911c9eb0a1b99b08d57bd7bc6
Jollen Chen <jollen@moko365.com>
2014-07-27 10:26:27 +0800

    use requirejs.config

--- javascripts/loader.js
+++ javascripts/loader.js
@@ -1,5 +1,15 @@
+requirejs.config({
+	baseUrl: 'bower_components/',
+	paths: {
+		'jquery': 'jquery/jquery.min'
+	}
+});
+
 require([
 	/* by order */
+	'jquery',
+	'bower_components/jquery-ui/ui/jquery-ui.js',
+	'bower_components/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js',
 	'javascripts/main.js'
 ], function () {
 	console.log('Loaded.');
